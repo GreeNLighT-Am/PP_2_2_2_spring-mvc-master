57257a1d1fc82cb3dfe323c9db3f7a5545b1501f
Vladislav Gutnik <lt38igreen@gmail.com>
2026-03-13 23:28:14 +0300

    Job done

---
+++ src/main/java/web/controller/CarController.java
@@ -0,0 +1,38 @@
+package web.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.ModelMap;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import web.model.Car;
+import web.service.CarService;
+
+import java.util.List;
+
+@Controller
+public class CarController {
+
+    @Autowired
+    private CarService carService;
+
+
+    @GetMapping("/cars")
+    public String getCars(@RequestParam(value = "count", required = false) Integer count, ModelMap model) {
+
+        List<Car> cars = carService.getAllCars();
+
+        if (count != null) {
+            if (count < 0 || count == 0) {
+                model.addAttribute("error", "Ошибка: значение count не может быть 0 или отрицательным.");
+                return "cars";
+            } else {
+                model.addAttribute("cars", carService.getCarsByCount(cars, count));
+            }
+        } else {
+            model.addAttribute("cars", cars);
+        }
+
+        return "cars";
+    }
+}
